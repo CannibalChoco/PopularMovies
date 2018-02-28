da6ee8346fd8e949b23941bf652f5971abbab601
CannibalChoco <fudgeblimp@gmail.com>
2018-02-28 14:01:26 +0200

    Add internet permission; check json response with asyncTask

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.android.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.popularmovies.Utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity{
+
+    private String apiKey;
+
+    private TextView sampleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiKey = getString(R.string.API_KEY);
+
+        sampleText = findViewById(R.id.sampleText);
+
+        new MovieQueryTask().execute();
+    }
+
+    public class MovieQueryTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... voids) {
+            URL url = NetworkUtils.buildUrl(NetworkUtils.PATH_TOP_RATED, apiKey);
+            String jsonResponse = "";
+            try {
+                jsonResponse = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (s != null && !s.equals("")) {
+                sampleText.setText(s);
+            }
+        }
     }
 }
