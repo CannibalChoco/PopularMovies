16e9464da6e28acd04eaf86209a1a96516bfbee8
CannibalChoco <fudgeblimp@gmail.com>
2018-03-05 17:12:49 +0200

    Display movie info on DetailActivity

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.android.popularmovies.Utils.NetworkUtils;
@@ -14,12 +15,15 @@ import com.squareup.picasso.Picasso;
  * Created by Emils on 26.02.2018.
  */
 
-// TODO: fix costraintLayouts margins issue for tvOverview
 public class DetailActivity extends AppCompatActivity {
 
     private TextView titleTv;
     private TextView overviewTv;
     private ImageView posterIv;
+    private TextView ratingTv;
+    private TextView releaseDateTv;
+    private RatingBar ratingBar;
+    private TextView languageTv;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,14 +39,28 @@ public class DetailActivity extends AppCompatActivity {
         titleTv = findViewById(R.id.tvTitle);
         overviewTv = findViewById(R.id.tvOverview);
         posterIv = findViewById(R.id.ivPoster);
+        ratingTv = findViewById(R.id.tvRating);
+        releaseDateTv = findViewById(R.id.tvReleaseDate);
+        ratingBar = findViewById(R.id.ratingBar);
+        languageTv = findViewById(R.id.tvLanguage);
 
         String title = movie.getMovieTitle();
         String overview = movie.getOverview();
         String posterPath = movie.getPosterPath();
         String posterUrl = NetworkUtils.buildUrlForMoviePoster(posterPath);
+        String rating = movie.getRatingString();
+        String releaseDate = movie.getReleaseYear();
+        String language = movie.getLanguage();
+
+        float ratingFloat = Float.valueOf(rating);
 
         titleTv.setText(title);
         overviewTv.setText(overview);
+        ratingTv.setText(rating);
+        releaseDateTv.setText(releaseDate);
+        ratingBar.setRating(ratingFloat);
+        languageTv.setText(language);
+
         Picasso.with(this).load(posterUrl).into(posterIv);
 
     }
--- app/src/main/java/com/example/android/popularmovies/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -4,6 +4,9 @@ package com.example.android.popularmovies;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 /**
  * Created by Emils on 26.02.2018.
  */
@@ -13,7 +16,7 @@ public class Movie implements Parcelable {
     private String title;
     private String overview;
     private String posterPath;
-    private String releaseYear;
+    private String releaseDate;
     private double rating;
     private String language;
 
@@ -22,7 +25,7 @@ public class Movie implements Parcelable {
         this.title = title;
         this.overview = overview;
         this.posterPath = posterPath;
-        this.releaseYear = getYearFromDate(releaseDate);
+        this.releaseDate = releaseDate;
         this.rating = rating;
         this.language = language;
     }
@@ -31,7 +34,7 @@ public class Movie implements Parcelable {
         title = in.readString();
         overview = in.readString();
         posterPath = in.readString();
-        releaseYear = in.readString();
+        releaseDate = in.readString();
         rating = in.readDouble();
         language = in.readString();
     }
@@ -61,20 +64,27 @@ public class Movie implements Parcelable {
         return posterPath;
     }
 
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
     public String getReleaseYear() {
-        return releaseYear;
+        return releaseDate.substring(0, 4);
     }
 
     public double getRating() {
         return rating;
     }
 
-    public String getLanguage() {
-        return language;
+    public String getRatingString(){
+        DecimalFormat df = new DecimalFormat("#.#");
+        df.setRoundingMode(RoundingMode.HALF_EVEN);
+
+        return df.format(getRating());
     }
 
-    public String getYearFromDate(String date){
-        return date.substring(0, 4);
+    public String getLanguage() {
+        return language;
     }
 
     /**
@@ -86,7 +96,7 @@ public class Movie implements Parcelable {
         String movieString = title + "; " + "\n" +
                             overview + "; " +  "\n" +
                             posterPath + "; " +  "\n" +
-                            releaseYear + "; " +  "\n" +
+                            releaseDate + "; " +  "\n" +
                             String.valueOf(rating) + "; " + "\n" +
                             language;
 
@@ -103,7 +113,7 @@ public class Movie implements Parcelable {
         dest.writeString(title);
         dest.writeString(overview);
         dest.writeString(posterPath);
-        dest.writeString(releaseYear);
+        dest.writeString(releaseDate);
         dest.writeDouble(rating);
         dest.writeString(language);
     }
